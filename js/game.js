70d56514e55b20ca82c997c13fe5c926dd7a0bcd
STOKYS <57591015+STOKYS@users.noreply.github.com>
2021-01-19 19:16:40 +0100

    Update v21.0119:20
    Updated images, icon. Fully working game, little bit more responsive

--- js/game.js
+++ js/game.js
@@ -1,110 +1,240 @@
 class Game {
-    constructor(){
-        this.game_started = true;
+    constructor() {
+        this.game_started = false;
+        this.snake = [new Head(Math.floor((Math.random() * ((canvas.width - 25) - 0) + 0) / 25) * 25, Math.floor((Math.random() * ((canvas.height - 25) - 0) + 0) / 25) * 25, 25, 25)]
+        this.food = new Food(Math.floor((Math.random() * ((canvas.width - 25) - 0) + 0) / 25) * 25, Math.floor((Math.random() * ((canvas.height - 25) - 0) + 0) / 25) * 25, 25, 25)
+        this.score = 0;
+        this.changed = false;
+        ctx.font = "40px Verdana";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center";
+        ctx.fillText(`Move to start (WASD)`, canvas.width / 2, canvas.height / 2);
     }
-    start(){
+    start() {
+        this.game_started = true;
         update()
     }
-    update(){
-        console.log("hey")
-        ctx.clearRect(0,0,canvas.width,canvas.height)
-        if (snake.length != 1 && snake[0].move != ''){
-            update_under()
-        } else {
-            snake[0].update()
+    update() {
+        this.score = this.snake.length
+        this.changed = false;
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        for (let i = (this.snake.length - 1); i > 0; i--) {
+            this.snake[i].update(i)
         }
-        food.update()
+        for (let i = (this.snake.length - 1); i > 0; i--) {
+            this.snake[i].draw(i)
+        }
+        this.snake[0].update()
+        this.food.draw()
+        this.gui()
+    }
+    gui() {
+        ctx.font = "15px Verdana";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center";
+        ctx.fillText(`Score: ${this.score}`, 40, 20);
+    }
+    over() {
+        game.game_started = false
+        ctx.font = "80px Verdana";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center";
+        ctx.fillText(`Game Over!`, canvas.width / 2, (canvas.height / 2) - 100);
+        clensing()
     }
 }
 
-class Snake {
-    constructor(x,y,width,height){
+class Head {
+    constructor(x, y, width, height) {
         this.x = x
         this.y = y
         this.width = width
         this.height = height
         this.speed = this.width
-        this.move = ''
+        this.move = 'up'
     }
-    update(){
-        if (this.move != ''){
-            if (this.move == "up"){
+    update() {
+        if (this.move != '') {
+            if (this.move == "up") {
                 this.y -= this.speed
-            } else if (this.move == "down"){
+            } else if (this.move == "down") {
                 this.y += this.speed
-            } else if (this.move == "right"){
+            } else if (this.move == "right") {
                 this.x += this.speed
-            } else if (this.move == "left"){
+            } else if (this.move == "left") {
                 this.x -= this.speed
             }
         }
-        snake.forEach(function(obj){
-            obj.draw()
-        })
-        snake[0].colision()
+        this.walls()
+        this.colision()
+        this.draw()
     }
-    draw(){
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.x,this.y,this.width,this.height)
+    draw() {
+        let image = this.image()
+        ctx.drawImage(image, this.x, this.y, this.width, this.height)
     }
-    colision(){
-        snake.forEach(function(obj, index){
-            if (obj.x == snake[0].x && obj.y == snake[0].y && index != 0){
-                game.game_started = false
-                ctx.font = "80px Verdana";
-                ctx.lineWidth = 2;
-                ctx.fillStyle = "white";
-                ctx.strokeStyle = "black"
-                ctx.textAlign = "center";
-                ctx.fillText(`Game Over!`, canvas.width/2, canvas.height/2);
-                ctx.strokeText(`Game Over!`, canvas.width/2, canvas.height/2);
+    image() {
+        if (this.move == "up") {
+            return hw
+        } else if (this.move == "down") {
+            return hs
+        } else if (this.move == "left") {
+            return ha
+        } else if (this.move == "right") {
+            return hd
+        }
+    }
+    colision() {
+        // Kolize se samo sebou
+        game.snake.forEach(function (obj, index) {
+            if (obj.x == game.snake[0].x && obj.y == game.snake[0].y && index != 0) {
+                game.over()
             }
         })
         this.eat()
     }
-    eat(){
-        if (snake[0].x == food.x && snake[0].y == food.y){
-            food.create()
-            snake.push(new Snake(snake[snake.length - 1].x, snake[snake.length - 1].y, snake[snake.length - 1].width, snake[snake.length - 1].height))
+    walls() {
+        // Teleportace
+        if (options.dw) {
+            if (this.x < 0 || this.x >= canvas.width || this.y < 0 || this.y >= canvas.height) game.over()
+        } else {
+            if (this.x < 0) {
+                this.x = canvas.width - 25
+            } else if (this.x >= canvas.width) {
+                this.x = 0
+            } else if (this.y < 0) {
+                this.y = canvas.height - 25
+            } else if (this.y >= canvas.height) {
+                this.y = 0
+            }
+        }
+    }
+    eat() {
+        if (this.x == game.food.x && this.y == game.food.y) {
+            game.food.create()
+            game.snake.push(new Body(game.snake[game.snake.length - 1].x, game.snake[game.snake.length - 1].y, game.snake[game.snake.length - 1].width, game.snake[game.snake.length - 1].height))
         }
     }
 }
 
-class Food {
-    constructor(x,y,width,height){
+class Body {
+    constructor(x, y, width, height) {
         this.x = x
         this.y = y
         this.width = width
         this.height = height
     }
-    create(){
-        let x = Math.floor((Math.random() * ((canvas.width - 25) - 0) + 0) / 25) * 25
-        let y = Math.floor((Math.random() * ((canvas.height - 25) - 0) + 0)  / 25) * 25
-        console.log(x + " " + y)
-        this.x = x
-        this.y = y
+    update(index) {
+        this.x = game.snake[index - 1].x
+        this.y = game.snake[index - 1].y
     }
-    update(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.x,this.y,this.width,this.height)
+    draw(index) {
+        let image = this.image(index)
+        ctx.drawImage(image, this.x, this.y, this.width, this.height)
+    }
+    image(index) {
+        // Nemá žádnou reálnou funkci, pouze zjišťuje vhodý obrázek
+        if (index == (game.snake.length - 1)) {
+            if (game.snake.length == 2) {
+                if (game.snake[0].move == "up") {
+                    return tw
+                } else if (game.snake[0].move == "down") {
+                    return ts
+                } else if (game.snake[0].move == "left") {
+                    return ta
+                } else if (game.snake[0].move == "right") {
+                    return td
+                }
+            } else {
+                if (game.snake[index - 1].x == this.x && game.snake[index - 1].y > this.y) {
+                    return ts
+                } else if (game.snake[index - 1].x == this.x && game.snake[index - 1].y < this.y) {
+                    return tw
+                } else if (game.snake[index - 1].x > this.x && game.snake[index - 1].y == this.y) {
+                    return td
+                } else if (game.snake[index - 1].x < this.x && game.snake[index - 1].y == this.y) {
+                    return ta
+                }
+            }
+        } else if (index == 1) {
+            if (game.snake[0].move == "up") {
+                if (game.snake[2].x > this.x) {
+                    return wd
+                } else if (game.snake[2].x < this.x) {
+                    return wa
+                } else if (game.snake[2].x == this.x) {
+                    return vertical
+                }
+            } else if (game.snake[0].move == "down") {
+                if (game.snake[2].x > this.x) {
+                    return sd
+                } else if (game.snake[2].x < this.x) {
+                    return sa
+                } else if (game.snake[2].x == this.x) {
+                    return vertical
+                }
+            } else if (game.snake[0].move == "left") {
+                if (game.snake[2].y > this.y) {
+                    return sa
+                } else if (game.snake[2].y < this.y) {
+                    return wa
+                } else if (game.snake[2].y == this.y) {
+                    return horizontal
+                }
+            } else if (game.snake[0].move == "right") {
+                if (game.snake[2].y > this.y) {
+                    return sd
+                } else if (game.snake[2].y < this.y) {
+                    return wd
+                } else if (game.snake[2].y == this.y) {
+                    return horizontal
+                }
+            }
+        } else {
+            if ((game.snake[index - 1].x == this.x && game.snake[index - 1].y < this.y) && (game.snake[index + 1].x < this.x && game.snake[index + 1].y == this.y) || (game.snake[index - 1].x < this.x && game.snake[index - 1].y == this.y) && (game.snake[index + 1].x == this.x && game.snake[index + 1].y < this.y)) {
+                return wa
+            } else if ((game.snake[index - 1].x == this.x && game.snake[index - 1].y < this.y) && (game.snake[index + 1].x > this.x && game.snake[index + 1].y == this.y) || (game.snake[index - 1].x > this.x && game.snake[index - 1].y == this.y) && (game.snake[index + 1].x == this.x && game.snake[index + 1].y < this.y)) {
+                return wd
+            } else if ((game.snake[index - 1].x == this.x && game.snake[index - 1].y > this.y) && (game.snake[index + 1].x < this.x && game.snake[index + 1].y == this.y) || (game.snake[index - 1].x < this.x && game.snake[index - 1].y == this.y) && (game.snake[index + 1].x == this.x && game.snake[index + 1].y > this.y)) {
+                return sa
+            } else if ((game.snake[index - 1].x == this.x && game.snake[index - 1].y > this.y) && (game.snake[index + 1].x > this.x && game.snake[index + 1].y == this.y) || (game.snake[index - 1].x > this.x && game.snake[index - 1].y == this.y) && (game.snake[index + 1].x == this.x && game.snake[index + 1].y > this.y)) {
+                return sd
+            } else if (game.snake[index - 1].y == this.y && game.snake[index + 1].y == this.y) {
+                return horizontal
+            } else if (game.snake[index - 1].x == this.x && game.snake[index + 1].x == this.x) {
+                return vertical
+            }
+        }
     }
 }
 
-function update_under(){
-    for (let i = (snake.length - 1); i >= 0; i--){
-        if(i == 0){
-            snake[0].update()
-        } else {
-            snake[i].x = snake[i-1].x
-            snake[i].y = snake[i-1].y
-        }
+class Food {
+    constructor(x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+    create() {
+        // Znovu vytvoří jídlo na plátně v náhodné pozici
+        this.x = Math.floor((Math.random() * ((canvas.width - 25) - 0) + 0) / 25) * 25
+        this.y = Math.floor((Math.random() * ((canvas.height - 25) - 0) + 0) / 25) * 25
+        // Zjišťuje zda se jídlo nevytvořilo uvnitř hada, pokud ano: funkce se opakuje
+        game.snake.forEach(function (obj) {
+            if (obj.x == game.food.x && obj.y == game.food.y) {
+                game.food.create()
+            }
+        })
+    }
+    draw() {
+        ctx.drawImage(rodent, this.x, this.y, this.width, this.height)
     }
 }
 
 let timeOne = 0;
 
 function update() {
-    if ((Date.now() - timeOne) >= 500) {
+    if ((Date.now() - timeOne) >= ((options.hsm) ? 100 : 300)) {
         timeOne = Date.now()
         game.update()
     }
@@ -114,5 +244,8 @@ function update() {
 }
 
 let game = new Game()
-let snake = [new Snake(350, 350, 25, 25)]
-let food = new Food(100,100,25,25)
+
+// Vytvoří nový objekt hry
+function clensing() {
+    game = new Game()
+}
--- js/setup.js
+++ js/setup.js
@@ -2,35 +2,122 @@ const canvas = document.getElementById("game")
 const ctx = canvas.getContext('2d');
 ctx.globalCompositeOperation = "source-over";
 
-document.getElementById("start").addEventListener("click", function(){
-    game.start()
-})
+let options = {
+    hrm: false,
+    hsm: false,
+    dw: false,
+    skin: "smooth"
+}
+
+
+const horizontal = new Image()
+const vertical = new Image()
+const wa = new Image()
+const wd = new Image()
+const sa = new Image()
+const sd = new Image()
+const ta = new Image()
+const td = new Image()
+const tw = new Image()
+const ts = new Image()
+const ha = new Image()
+const hd = new Image()
+const hw = new Image()
+const hs = new Image()
+
+horizontal.src = `img/${options.skin}/horizontal.png`
+vertical.src = `img/${options.skin}/vertical.png`
+wa.src = `img/${options.skin}/wa.png`
+wd.src = `img/${options.skin}/wd.png`
+sa.src = `img/${options.skin}/sa.png`
+sd.src = `img/${options.skin}/sd.png`
+ta.src = `img/${options.skin}/ta.png`
+td.src = `img/${options.skin}/td.png`
+tw.src = `img/${options.skin}/tw.png`
+ts.src = `img/${options.skin}/ts.png`
+ha.src = `img/${options.skin}/ha.png`
+hd.src = `img/${options.skin}/hd.png`
+hw.src = `img/${options.skin}/hw.png`
+hs.src = `img/${options.skin}/hs.png`
+
+const rodent = new Image()
+rodent.src = "img/rodent.png"
 
 document.onkeyup = function movement(key) {
     if (game.game_started == true) {
         switch (key.keyCode) {
             case 87:
-                if (snake[0].move != "down"){
-                    snake[0].move = "up";
+                if (game.snake[0].move != "down" && (!game.changed || options.hrm)) {
+                    game.snake[0].move = "up";
+                    game.changed = true
                 }
                 break;
             case 83:
-                if (snake[0].move != "up"){
-                    snake[0].move = "down";
+                if (game.snake[0].move != "up" && (!game.changed || options.hrm)) {
+                    game.snake[0].move = "down";
+                    game.changed = true
                 }
 
                 break;
             case 68:
-                if (snake[0].move != "left"){
-                    snake[0].move = "right";
+                if (game.snake[0].move != "left" && (!game.changed || options.hrm)) {
+                    game.snake[0].move = "right";
+                    game.changed = true
                 }
 
                 break;
             case 65:
-                if (snake[0].move != "right"){
-                    snake[0].move = "left";
+                if (game.snake[0].move != "right" && (!game.changed || options.hrm)) {
+                    game.snake[0].move = "left";
+                    game.changed = true
                 }
                 break;
         }
+    } else {
+        if (key.key == "w" || key.key == "s" || key.key == "d" || key.key == "a") {
+            game.start()
+        }
     }
+}
+
+document.getElementById("hrm").addEventListener("click", function () {
+    options.hrm = !options.hrm
+    this.innerText = `High Response Mode: ${options.hrm}`
+})
+
+document.getElementById("hsm").addEventListener("click", function () {
+    options.hsm = !options.hsm
+    this.innerText = `High Speed Mode: ${options.hsm}`
+})
+
+document.getElementById("dw").addEventListener("click", function () {
+    options.dw = !options.dw
+    this.innerText = `Deadly walls: ${options.dw}`
+})
+
+document.getElementById("skin").addEventListener("click", function () {
+    if (options.skin == "smooth") {
+        options.skin = "tiled"
+    } else if (options.skin == "tiled") {
+        options.skin = "smooth"
+    }
+    this.innerText = `Skin: ${options.skin}`
+    skin()
+})
+
+function skin() {
+    horizontal.src = `img/${options.skin}/horizontal.png`
+    vertical.src = `img/${options.skin}/vertical.png`
+    wa.src = `img/${options.skin}/wa.png`
+    wd.src = `img/${options.skin}/wd.png`
+    sa.src = `img/${options.skin}/sa.png`
+    sd.src = `img/${options.skin}/sd.png`
+    ta.src = `img/${options.skin}/ta.png`
+    td.src = `img/${options.skin}/td.png`
+    tw.src = `img/${options.skin}/tw.png`
+    ts.src = `img/${options.skin}/ts.png`
+    ha.src = `img/${options.skin}/ha.png`
+    hd.src = `img/${options.skin}/hd.png`
+    hw.src = `img/${options.skin}/hw.png`
+    hs.src = `img/${options.skin}/hs.png`
 }
